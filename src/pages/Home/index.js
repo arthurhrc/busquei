5d46dd32e65dac620a6c9acf9710687f3f05ea49
arthurhrc <arthurhrc@gmail.com>
2026-04-11 18:01:47 -0300

    style: remover links sem destino e refinir histórico de buscas

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 import Search from '../../components/Search/index';
 import Busquei from '../../assets/busquei.png';
 import AppsIcon from '@material-ui/icons/Apps';
-import HistoryIcon from '@material-ui/icons/History';
 import Brightness4Icon from '@material-ui/icons/Brightness4';
 import Brightness7Icon from '@material-ui/icons/Brightness7';
 import { Avatar, IconButton } from '@material-ui/core';
@@ -27,13 +26,8 @@ function Home() {
             <div className="home">
 
                 <div className="home__header">
-                    <div className="home__headerLeft">
-                        <Link to="/about">Sobre</Link>
-                        <Link to="/loja">Loja</Link>
-                    </div>
+                    <div className="home__headerLeft" />
                     <div className="home__headerRight">
-                        <Link to="/email">E-mail</Link>
-                        <Link to="/imagens">Imagens</Link>
                         <AppsIcon />
                         <IconButton
                             size="small"
@@ -54,14 +48,12 @@ function Home() {
 
                     {searchHistory.length > 0 && (
                         <div className="home__history">
-                            <span className="home__historyLabel">Pesquisas recentes</span>
                             {searchHistory.map(item => (
                                 <button
                                     key={item}
                                     className="home__historyItem"
                                     onClick={() => handleHistorySearch(item)}
                                 >
-                                    <HistoryIcon className="home__historyIcon" />
                                     {item}
                                 </button>
                             ))}
